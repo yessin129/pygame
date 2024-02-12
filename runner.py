475cf6d00a9a4917aff73e1b6189cd7480daa496
jessin129 <z.jessin129@gmail.com>
2024-02-12 11:23:55 +0100

    .
    .

--- runner.py
+++ runner.py
@@ -1,18 +1,22 @@
 
 import pygame
+from sys import exit     
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("runner")
 clock = pygame.time.Clock()
 
-test_surface= pygame.Surface((100,200))
+test_surface = pygame.Surface((100,200))
 test_surface.fill('red')
+
 while True:
-     for event in pygame.event.get():
-          if event.type == pygame.QUIT:
-           pygame.quit()
-           exit()
-       #screen.blit(test_surface,(0,0))
-     pygame.display.update
-     clock.tick(60)
+          for event in pygame.event.get():
+           if event.type == pygame.QUIT:
+             pygame.quit()
+             exit()
+           screen.blit(test_surface,(200,100))
+
+      
+           pygame.display.update()
+           clock.tick(60)

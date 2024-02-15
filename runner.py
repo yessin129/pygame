2c313c283ca606b303b561aafb28b9fa6fe447ce
yessin129 <jessin129@gmail.com>
2024-02-15 08:49:47 +0100

    .
    .

--- runner.py
+++ runner.py
@@ -5,14 +5,23 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("runner")
 clock = pygame.time.Clock()
+test_font = pygame.font.Font("font/pixeltype.ttf",50)
 
-test_surface= pygame.Surface((100,200))
-test_surface.fill('red')
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
+text_surface = test_font.render("My game",False,"black")
+
+snail_sufrace = pygame.image.load('graphics/snail/snail1.png')
+snail_x_pos = 600
 while True:
      for event in pygame.event.get():
           if event.type == pygame.QUIT:
            pygame.quit()
            exit()
-     screen.blit(test_surface,(0,0))
+     screen.blit(sky_surface,(0,0))
+     screen.blit(ground_surface,(0,300))
+     screen.blit(text_surface,(300,50))
+     snail_x_pos -= 1
+     screen.blit(snail_sufrace,(snail_x_pos,250))
      pygame.display.update()
      clock.tick(60)

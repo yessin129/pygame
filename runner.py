7a4d6903801948fa97a741b5d3fa3954bbc4ce5e
yessin129 <jessin129@gmail.com>
2024-03-13 12:21:08 +0100

    .
    .

--- runner.py
+++ runner.py
@@ -13,22 +13,40 @@ sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface = test_font.render("My game",False,"black")
 
-snail_sufrace = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_x_pos = 600
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surface.get_rect(bottomright = (600,300))
+
 
 player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surface.get_rect(midbottom = (80,300))
 while True:
      for event in pygame.event.get():
           if event.type == pygame.QUIT:
            pygame.quit()
-           exit()
+           exit() 
+          if event.type == pygame.M:
+             print(event.pos)
+             
+
      screen.blit(sky_surface,(0,0))
      screen.blit(ground_surface,(0,300))
      screen.blit(text_surface,(300,50))
-     snail_x_pos -= 4
-     if snail_x_pos <-100: snail_x_pos = 800
-     screen.blit(snail_sufrace,(snail_x_pos,250))
-     screen.blit(player_surface,(80,200))
+     
+     snail_rect.x -= 4
+     if snail_rect.right <= 0: snail_rect.left = 800
+     screen.blit(snail_surface,snail_rect)
+     player_rect.left += 1
+     screen.blit(player_surface,player_rect)
+
+     #if (player_rect.colliderect(snail_rect)):
+       #  print ("collision")
+     mouse_pos = pygame.mouse.get_pos()
+     if player_rect.collidepoint(mouse_pos):
+       print (pygame.mouse.get_pressed())
+
+
+     
+
      pygame.display.update()
      clock.tick(60)
 

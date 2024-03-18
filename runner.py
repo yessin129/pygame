a9575e9be09a69b207424abf9e392881786be809
yessin129 <jessin129@gmail.com>
2024-03-18 09:43:00 +0100

    Update runner.py

--- runner.py
+++ runner.py
@@ -11,7 +11,10 @@ test_font = pygame.font.Font("font/pixeltype.ttf",50)
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-text_surface = test_font.render("My game",False,"black")
+
+
+score_surf = test_font.render("my game",False,"black")
+score_rect = score_surf.get_rect(center = (400,50))
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(bottomright = (600,300))
@@ -19,18 +22,22 @@ snail_rect = snail_surface.get_rect(bottomright = (600,300))
 
 player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surface.get_rect(midbottom = (80,300))
+      
 while True:
      for event in pygame.event.get():
           if event.type == pygame.QUIT:
            pygame.quit()
            exit() 
-          if event.type == pygame.M:
-             print(event.pos)
+          if event.type == pygame.MOUSEMOTION:
+              if player_rect.collidepoint(event.pos): print ("collision")
              
 
      screen.blit(sky_surface,(0,0))
      screen.blit(ground_surface,(0,300))
-     screen.blit(text_surface,(300,50))
+     pygame.draw.rect(screen,'#c0e8ec',score_rect)
+     pygame.draw.rect(screen,'#c0e8ec',score_rect,10)
+    
+     screen.blit(score_surf,score_rect)
      
      snail_rect.x -= 4
      if snail_rect.right <= 0: snail_rect.left = 800
@@ -40,13 +47,13 @@ while True:
 
      #if (player_rect.colliderect(snail_rect)):
        #  print ("collision")
-     mouse_pos = pygame.mouse.get_pos()
-     if player_rect.collidepoint(mouse_pos):
-       print (pygame.mouse.get_pressed())
+     #mouse_pos = pygame.mouse.get_pos()
+    # if player_rect.collidepoint(mouse_pos):
+      # print (pygame.mouse.get_pressed())
 
 
      
 
      pygame.display.update()
      clock.tick(60)
-
+    

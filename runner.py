6c9e440b40a48ec91552c6906ca717854f4b3a01
yessin129 <jessin129@gmail.com>
2024-04-05 11:14:00 +0200

    .
    .

--- runner.py
+++ runner.py
@@ -1,4 +1,14 @@
 
+
+
+def display_score():
+    current_time =  int(pygame.time.get_ticks () / 1000) - start_time
+    score_surf = test_font.render(f'{current_time}',False,(64,64,64))
+    score_rect = score_surf.get_rect(center = (400,50))
+    screen.blit(score_surf,score_rect)
+    print(current_time)
+
+
 import pygame
 from sys import exit     
 
@@ -8,14 +18,15 @@ pygame.display.set_caption("runner")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("font/pixeltype.ttf",50)
 game_active = True
+start_time = 0
 
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
 
-score_surf = test_font.render("my game",False,"black")
-score_rect = score_surf.get_rect(center = (400,50))
+#score_surf = test_font.render("my game",False,(64,64,64))
+#score_rect = score_surf.get_rect(center = (400,50))
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(bottomright = (600,300))
@@ -30,20 +41,27 @@ while True:
           if event.type == pygame.QUIT:
            pygame.quit()
            exit() 
-          if event.type == pygame.MOUSEBUTTONDOWN:
-              if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300: 
+
+          if game_active:
+           if event.type == pygame.MOUSEBUTTONDOWN:
+               if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300: 
                  player_gravity = -20
 
-          if event.type == pygame.KEYDOWN:
-              if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+           if event.type == pygame.KEYDOWN:
+               if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
                     player_gravity = -20   
-             
+          else: 
+             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                snail_rect.left = 800     
+                start_time = int (pygame.time.get_ticks() / 1000)
      if game_active:
         screen.blit(sky_surface,(0,0))
         screen.blit(ground_surface,(0,300))
-        pygame.draw.rect(screen,'#c0e8ec',score_rect)
-        pygame.draw.rect(screen,'#c0e8ec',score_rect,10)
-        screen.blit(score_surf,score_rect)
+        #pygame.draw.rect(screen,'#c0e8ec',score_rect)
+        #pygame.draw.rect(screen,'#c0e8ec',score_rect,10)
+        #screen.blit(score_surf,score_rect)
+        display_score()
         
         snail_rect.x -= 4
         if snail_rect.right <= 0: snail_rect.left = 800
